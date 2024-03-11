23aa32c8935081fd17d1d66b59111a043253fee1
shaukat_abedi@hotmail.com <shaukat_abedi@hotmail.com>
2024-03-11 17:11:54 +1100

    leafletAndBarGraphs

--- run-webserver.py
+++ run-webserver.py
@@ -10,5 +10,18 @@ def index():
 def animatecircle():
     return render_template('animateCircles-v1.html')
 
+@app.route('/slider')
+def slider():
+    return render_template('circleposAndSlider-v1.html')
+
+
+@app.route('/sliderdropdown')
+def sliderdropdown():
+    return render_template('circlepos-slider-dropdown-v1.html')
+
+@app.route('/leafletmap')
+def leafletmap():
+    return render_template('leafletAndGraph-v1.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
