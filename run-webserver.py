d6b46a88a7713cb68b5fd3776b284606aad3d8ec
shaukat_abedi@hotmail.com <shaukat_abedi@hotmail.com>
2024-03-11 11:20:07 +1100

    visualizations and webserver - v1

---
+++ run-webserver.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<p>Hello!</p>"
+
+@app.route('/')
+def index():
+    return render_template('animateCircles-v1.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
