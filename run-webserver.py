40e1263f1a9edaa9584f90c2aa7c24c3249e2abf
Shaukat Abidi <shaukat@livdv-sweridev1.intra.swsahs.nsw.gov.au>
2024-03-13 14:06:27 +1100

    leafletAndD3 Testing

--- run-webserver.py
+++ run-webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -22,6 +22,18 @@ def sliderdropdown():
 @app.route('/leafletmap')
 def leafletmap():
     return render_template('leafletAndGraph-v3.html')
+
+@app.route('/leafletd3')
+def leafletd3():
+    return render_template('leafletAndD3-v1.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.route('/data/<path:filename>')
+def download_file(filename):
+    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
